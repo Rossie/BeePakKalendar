9c954ff948e5abcf0ab6667474394660388f3e3c
Rossie <rossie.g@gmail.com>
2018-01-05 00:54:27 +0100

    Removed 'platforms' folder

--- platforms/android/app/src/main/assets/app/tns_modules/nativescript-imagecropper/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { OptionsCommon } from './interfaces';
-import { Result } from './interfaces';
-import * as imageSource from 'tns-core-modules/image-source';
-
-export declare class ImageCropper {
-    show(image: imageSource.ImageSource, options?: OptionsCommon): Promise<Result>;    
-}
